1536b9ad51764534ec853695fd158da590f36348
Daniel151505 <daniel150520011@gmail.com>
2023-10-18 07:16:06 -0500

    Comenzando la funcionalidad para resetear passwords

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -70,4 +70,22 @@ const confirmar = async (req, res) => {
   }
 };
 
-export { registrar, autenticar, confirmar };
+const olvidePassword = async (req, res) => {
+  const { email } = req.body;
+  //Comprobar si el usuario existe
+  const usuario = await Usuario.findOne({ email });
+  if (!usuario) {
+    const error = new Error("El Usuario no existe");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  try {
+    usuario.token = generarId();
+    await usuario.save();
+    res.json({ msg: "Hemos enviado un email con las instrucciones" });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { registrar, autenticar, confirmar, olvidePassword };
--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -5,11 +5,13 @@ import {
   autenticar,
   registrar,
   confirmar,
+  olvidePassword,
 } from "../controllers/usuarioController.js";
 
 // Autenticación, Registro y Confirmación de Usuarios
 router.post("/", registrar); // Crea un nuevo usuario
 router.post("/login", autenticar);
 router.post("/confirmar/:token", confirmar);
+router.post("/olvide-password", olvidePassword);
 
 export default router;
