4bca6679aa893755449408aa78556785ebaa2e1b
Daniel151505 <daniel150520011@gmail.com>
2023-10-18 17:53:16 -0500

    Creando Proyectos

--- controllers/proyectoController.js
+++ controllers/proyectoController.js
@@ -1,6 +1,18 @@
+import Proyecto from "../models/Proyecto.js";
+
 const obtenerProyectos = async (req, res) => {};
 
-const nuevoProyecto = async (req, res) => {};
+const nuevoProyecto = async (req, res) => {
+  const proyecto = new Proyecto(req.body);
+  proyecto.creador = req.usuario._id;
+
+  try {
+    const proyectoAlmacenado = await proyecto.save();
+    res.json(proyectoAlmacenado);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const obtenerProyecto = async (req, res) => {};
 
@@ -13,14 +25,14 @@ const agregarColaborador = async (req, res) => {};
 const eliminarColaborador = async (req, res) => {};
 
 const obtenerTareas = async (req, res) => {};
- 
+
 export {
-    obtenerProyectos,
-    nuevoProyecto,
-    obtenerProyecto,
-    editarProyecto,
-    eliminarProyecto,
-    agregarColaborador,
-    eliminarColaborador,
-    obtenerTareas
-}
+  obtenerProyectos,
+  nuevoProyecto,
+  obtenerProyecto,
+  editarProyecto,
+  eliminarProyecto,
+  agregarColaborador,
+  eliminarColaborador,
+  obtenerTareas,
+};
