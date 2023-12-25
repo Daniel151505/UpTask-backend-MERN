6c6770cf9042b78e1d8290ccff27b8b09cec5f21
Daniel151505 <daniel150520011@gmail.com>
2023-12-24 19:54:40 -0500

    feat: Sending the welcome email

--- helpers/email.js
+++ helpers/email.js
@@ -1,5 +1,28 @@
-import nodemailer from "nodemailer"
+import nodemailer from "nodemailer";
 
-export const emailRegister = (data) => {
+export const emailRegister = async (data) => {
+  const { email, nombre, token } = data;
 
-}
+  const transport = nodemailer.createTransport({
+    host: "sandbox.smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "7a975c5543fdb3",
+      pass: "9657eccc323559",
+    },
+  });
+
+  // Email information
+
+  await transport.sendMail({
+    from: '"UpTask - Project Manager" <accounts@uptask.com>', 
+    to: email, 
+    subject: "Check Your Account", 
+    text: "Check Your Account on UpTask",
+    html: `
+        <p>Hello: ${nombre}, Check your account on UpTask </p>
+        <a href="${process.env.FRONTEND_URL}/confirm-account/${token}">Check Account</a>
+        <p> If you did not create this account, you can ignore the message </p>
+    `
+  })
+};
