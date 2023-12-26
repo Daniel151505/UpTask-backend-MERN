730a69e0b6088c8a67e09bb24a79cbbe5cae51bc
Daniel151505 <daniel150520011@gmail.com>
2023-12-26 00:44:37 -0500

    fix: Fix token

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -54,7 +54,7 @@ const autenticar = async (req, res) => {
       _id: usuario._id,
       nombre: usuario.nombre,
       email: usuario.email,
-      token: generarJWT(usuario_id),
+      token: generarJWT(usuario._id),
     });
   } else {
     const error = new Error("El password es incorrecto");
